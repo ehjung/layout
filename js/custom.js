29b0cde361904774f34341c7c6751417d24f64ef
ehjung <jesther0103@gmail.com>
2014-11-06 21:14:31 -0800

    Adding my own buttons

--- js/custom.js
+++ js/custom.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
 	//xPosition - Horizontal position of the element
 	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
 	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-	$('#intro').parallax("50%", 0.1);
-	$('#second').parallax("50%", 0.1);
+	$('#about_me').parallax("50%", 0.1);
+	$('#projects').parallax("50%", 0.1);
 	$('.bg').parallax("50%", 0.4);
-	$('#third').parallax("50%", 0.3);
+	$('#resume').parallax("50%", 0.3);
 
 });
