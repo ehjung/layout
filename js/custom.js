d2f8a43bfbbdb697b31daba2102acfe791a99d1e
ehjung <jesther0103@gmail.com>
2014-11-07 23:56:26 -0800

    Formatting the first section

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 	//xPosition - Horizontal position of the element
 	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
 	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-	$('#about_me').parallax("50%", 0.1);
+	//$('#about_me').parallax("50%", 0.1);
 	$('#projects').parallax("50%", 0.1);
 	$('.bg').parallax("50%", 0.4);
 	$('#resume').parallax("50%", 0.3);
